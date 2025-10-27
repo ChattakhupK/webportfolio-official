1ada4410a7d5892049a7617667c9808846f657e7
ChattakhupK <MosT5140@hotmail.com>
2025-10-27 08:55:49 +0700

    footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,17 +1,39 @@
 import React from "react";
 import ContainerBox from "../ContainerBox";
-import { TypographyH2, TypographyP2 } from "../Typography";
+import { TypographyH2, TypographyH3, TypographyP2 } from "../Typography";
 import Image from "next/image";
+import { FaFacebook, FaInstagram, FaGithub, FaDiscord } from "react-icons/fa";
+import Link from "next/link";
 import { Mail, Phone } from "lucide-react";
+
+const soical = [
+  {
+    icon: FaFacebook,
+    path: "https://www.facebook.com/MosT5140",
+  },
+  {
+    icon: FaInstagram,
+    path: "https://www.instagram.com/f.kykie",
+  },
+  {
+    icon: FaGithub,
+    path: "https://github.com/ChattakhupK",
+  },
+  {
+    icon: FaDiscord,
+    path: "https://discord.com/users/240118062827831297",
+  },
+];
 
 const Footer = () => {
   return (
     <div
-      className="text-(--primary-inverse) py-20 rounded-t-[150px]"
+      className="mx-auto w-full md:max-w-4/6  text-(--primary-inverse) py-10 rounded-t-[150px]"
       style={{ backgroundColor: "var(--bg-inverse)" }}
     >
       <ContainerBox>
-        <div className="flex justify-center -mt-15 mb-9">
+        {/* โลโก้ */}
+        <div className="flex justify-center -mt-10 mb-9">
           {/* โลโก้โหมดสว่าง */}
           <Image
             src="/logowhite-Photoroom.png"
@@ -29,16 +51,36 @@ const Footer = () => {
             className="hidden dark:block"
           />
         </div>
-        <div className="flex gap-x-10 justify-between items-center px-10">
+        {/* การติดต่อ */}
+        <div className="flex space-y-10 flex-col md:flex-row gap-x-10 justify-center items-start px-10">
           <div className="max-w-lg">
-            <TypographyH2>ช่องทางการติดต่อ</TypographyH2>
+            <div className="text-center md:text-start">
+              <TypographyH2>ช่องทางการติดต่อ</TypographyH2>
+            </div>
             <TypographyP2>
               ผมยินดีที่จะรับฟังความเห็นจากคุณ
               หรือการรับฟังความคิดเห็นเพื่อนำไปปรับปรุงผลงานของผมให้ดียิ่งขึ้น
             </TypographyP2>
-          </div>
-          <div className="max-w-lg">
-         asds
+            <div className="flex flex-col mt-2 space-y-1">
+              <div className="flex items-center">
+                <Mail /> | <TypographyP2>Most5140@hotmail.com</TypographyP2>
+              </div>
+              <div className="flex items-center">
+                <Phone /> | <TypographyP2>085-855-4668</TypographyP2>
+              </div>
+            </div>
+            <div className="flex gap-x-2 text-3xl mt-2">
+              {soical.map((link, index) => (
+                <Link
+                  className="hover:scale-110 duration-500 hover:text-red-700 dark:hover:text-red-300"
+                  key={index}
+                  target="_blank"
+                  href={link.path}
+                >
+                  <link.icon />
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </ContainerBox>
